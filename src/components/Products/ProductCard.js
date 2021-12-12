67df2677fc022843d1c90498f0f48e6dd43b2b18
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2021-12-11 21:17:02 -0500

    Creating card detail with attributes from parent

---
+++ src/components/Products/ProductCard.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+import DetailModal from "./DetailModal";
+
+const ProductCard = (props) => {
+    const [showInfo, setShowInfo] = useState(false);
+    return (
+        <>
+            <Card>
+                <Card.Img variant="top" src={props.image} />
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>{props.description}</Card.Text>
+                    <Button variant="primary">${props.price}</Button> {' '}
+                    <Button
+                        variant="outline-secondary"
+                        onClick={() => {setShowInfo(true); console.log(showInfo)}}
+                        
+                    >
+                        Detalles
+                    </Button>
+                    <DetailModal
+                        show={showInfo}
+                        title={props.name}
+                        image={props.image}
+                        onHide={() => setShowInfo(false)}
+                    />
+                </Card.Body>
+            </Card>
+        </>
+    );
+};
+
+export default ProductCard;
