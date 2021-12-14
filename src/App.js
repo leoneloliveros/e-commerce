26ca1bfedeabb892b990dccdf45aa892e41c3a59
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2021-12-14 17:56:39 -0500

    Renamend Landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavMenu from './components/NavMenu/NavMenu';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LandingPage from './pages/landing/Landing';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
