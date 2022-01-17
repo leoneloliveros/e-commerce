d493826dba123f65650bdd1cf308dd209d930541
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2022-01-17 18:35:40 -0500

    Some styles were fixed

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -6,16 +6,16 @@ const ProductCard = (props) => {
     const [showInfo, setShowInfo] = useState(false);
     return (
         <>
-            <Card>
+            <Card style={{height: '100%'}}>
                 <Card.Img variant="top" src={props.image} />
                 <Card.Body>
                     <Card.Title>{props.name}</Card.Title>
                     <Card.Text>{props.description}</Card.Text>
-                    <Button variant="primary">${props.price}</Button> {' '}
+                    <Button variant="primary" className="mb-1">${props.price}</Button> {' '}
                     <Button
                         variant="outline-secondary"
                         onClick={() => setShowInfo(true)}
-                        
+                        className="mb-1"
                     >
                         Detalles
                     </Button>
@@ -24,6 +24,7 @@ const ProductCard = (props) => {
                         title={props.name}
                         image={props.image}
                         onHide={() => setShowInfo(false)}
+                        
                     />
                 </Card.Body>
             </Card>
